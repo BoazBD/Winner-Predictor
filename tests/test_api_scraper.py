8571aa26ef2ed9a07443c71980316921176fda11
BoazBD <boaz@bardavid.com>
2024-03-30 18:40:42 +0300

    make test fail

--- tests/test_api_scraper.py
+++ tests/test_api_scraper.py
@@ -29,7 +29,7 @@ class TestApiScraper(unittest.TestCase):
         result = process_data(data)
         self.assertIsInstance(result, pd.DataFrame)
 
-        self.assertEqual(result.shape[0], 534)
+        self.assertEqual(result.shape[0], 533)
         self.assertEqual(result.shape[1], 11)
 
         not_nullable_columns = result.columns.difference(["option3", "ratio3"])
