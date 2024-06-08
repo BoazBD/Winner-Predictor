587e262da86517ea5049d2a7acc97c88c2c58980
BoazBD <boaz@bardavid.com>
2024-06-08 14:55:55 +0300

    add proxie server code

---
+++ api_request/oracle_proxy.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST"])
+def proxy_request():
+    target_url = request.json.get("url")
+    headers = request.json.get(
+        "headers", {}
+    )  # Default to empty dict if headers not provided
+
+    try:
+        response = requests.get(target_url, headers=headers)
+        response.raise_for_status()
+        return response.content
+    except Exception as e:
+        return str(e), 500
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
